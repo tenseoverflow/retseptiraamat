51f8ad12212e37b7f945adac4e33400a875de206
Henri Aunin <contact@hen.ee>
2025-03-30 23:33:40 +0300

    chore: remove unused backend files and update frontend navigation links

--- frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9c98dffbbd231dca37dcf5e920c0d2ae77b976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7568143ecae7d9aab5aafeb16a7cc6fcd6679b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
