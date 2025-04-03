1c2b33af7c2ab6f0290fc9790859c679219eca8a
Henri Aunin <contact@hen.ee>
2025-04-03 07:21:39 +0000

    chore: delete unneccessary Blazor app

--- frontend/Data/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    public DbSet<Recipe> Recipes { get; set; }
-    public DbSet<Ingredient> Ingredients { get; set; }
-}
